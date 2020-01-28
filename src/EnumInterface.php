b69945f5ba9f28ad44dc96a44889fc6ee459e747
Soroka Anton <uginroot@gmail.com>
2020-01-28 16:49:08 +0600

    Remove methods not concerning interface

--- src/EnumInterface.php
+++ src/EnumInterface.php
@@ -6,33 +6,9 @@ namespace Uginroot\PhpEnum;
 
 interface EnumInterface
 {
-    public function __toString();
-
     public function getName():string;
 
     public function getValue():int;
 
     public function isEqual(self $enum):bool;
-
-    /**
-     * @return string[]
-     */
-    public static function getNames():array;
-
-    /**
-     * @return int[]
-     */
-    public static function getValues():array;
-
-    /**
-     * @param int $value
-     * @return static
-     */
-    public static function createByValue(int $value);
-
-    /**
-     * @param string $name
-     * @return static
-     */
-    public static function createByName(string $name);
 }
